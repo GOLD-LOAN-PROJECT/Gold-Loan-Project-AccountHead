381ae2fd360753a1e263af5a4c766d6355939b2b
Patil-samiksha22 <samikshapatil873@gmail.com>
2021-10-18 16:50:21 +0530

    initial commit

---
+++ AccountHead_Gold_Loan_Project/src/test/java/com/main/AccountHeadGoldLoanProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountHeadGoldLoanProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
